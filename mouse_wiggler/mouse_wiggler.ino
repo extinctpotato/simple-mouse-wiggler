c589aeb08e01ded64709f319cb778865f0d3062f
Adam Olech <nddr89@gmail.com>
2022-04-16 18:10:00 +0200

    Modify pattern

--- mouse_wiggler/mouse_wiggler.ino
+++ mouse_wiggler/mouse_wiggler.ino
@@ -1,13 +1,21 @@
 #include "Mouse.h"
+bool direction = false;
 
 void setup() {
   Mouse.begin();
 }
 
 void loop() {
+  direction = !direction;
+
+  int directionMult = -1;
+
+  if (direction) {
+    directionMult = 1;
+  }
+
   for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      Mouse.move(i, j);
-    }
+    Mouse.move(10 * directionMult, 10 * directionMult);
+    delay(1000);
   }
 }
